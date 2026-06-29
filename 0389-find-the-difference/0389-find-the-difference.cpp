6a8605f6cdd6c00b284ae2daa06418616c3f8d06
Harsh Raj <129669805+HarshRajGH@users.noreply.github.com>
2026-06-29 14:30:09 +0530

    Time: 2 ms (16.23%), Space: 9.1 MB (78.06%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char result =0;
+        for(char c:s){
+            result= result ^c;
+        }
+        for(char c:t){
+            result= result ^c;
+        }
+        return result;
+    }
+};
