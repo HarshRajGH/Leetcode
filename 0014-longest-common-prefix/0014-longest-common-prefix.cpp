7f47054184175d4f52e48bdd0e5c14991e292afe
Harsh Raj <129669805+HarshRajGH@users.noreply.github.com>
2026-06-30 16:11:14 +0530

    Time: 0 ms (100%), Space: 12 MB (40.06%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = strs[0]; // Pehli string ko prefix maan liya
-        
+        string ans = strs[0]; 
         for (int i = 1; i < strs.size(); ++i) {
-            // Jab tak strs[i] ke shuru mein ans nahi milta (find != 0)
             while (strs[i].find(ans) != 0) {
-                // ans ko piche se ek character chota kar do
                 ans = ans.substr(0, ans.length() - 1);
                 if (ans.empty()) return "";
             }
         }
-        
         return ans;
     }
 };
