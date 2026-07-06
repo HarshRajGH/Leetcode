f3169b68e0c413eed980c9842aa36e073d87e3f1
Harsh Raj <129669805+HarshRajGH@users.noreply.github.com>
2026-07-06 14:12:28 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.39%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,22 +1,41 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         if (matrix.empty() || matrix[0].empty()) return false;
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         int low = 0;
+//         int high = (m * n) - 1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             int row = mid / n;
+//             int col = mid % n;
+//             int midElement = matrix[row][col];
+//             if (midElement == target) {
+//                 return true;
+//             } else if (midElement < target) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if (matrix.empty() || matrix[0].empty()) return false;
         int m = matrix.size();
         int n = matrix[0].size();
-        int low = 0;
-        int high = (m * n) - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int row = mid / n;
-            int col = mid % n;
-            int midElement = matrix[row][col];
-            if (midElement == target) {
-                return true;
-            } else if (midElement < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
             }
         }
         return false;
