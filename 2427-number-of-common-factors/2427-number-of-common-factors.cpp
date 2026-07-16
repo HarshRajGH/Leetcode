c1d85a5187b86a8d5be7e71aa134a34e3a859889
Harsh Raj <129669805+HarshRajGH@users.noreply.github.com>
2026-07-16 14:44:45 +0530

    Time: 0 ms (100%), Space: 7.8 MB (50.48%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int count = 0;
+        int limit = std::min(a, b); 
+        for (int i = 1; i <= limit; ++i) {
+            if (a % i == 0 && b % i == 0) {
+                count++;
+            }
+        }        
+        return count;
+    }
+};
